d5fae1f703e6623e6c35aee15e93d961aacb6a38
returnzork <returnzork@users.noreply.github.com>
2024-07-31 17:06:38 -0700

    Add project files.

---
+++ StardewValley-FlashingHarvest/ModEntry.cs
@@ -0,0 +1,14 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace returnzork.StardewValleyMod.FlashingHarvest
+{
+    internal class ModEntry : StardewModdingAPI.Mod
+    {
+        public override void Entry(IModHelper helper) => throw new NotImplementedException();
+    }
+}
