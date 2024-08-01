5e2659c44235e2cd5572c5de8a538d535bf65e0e
returnzork <returnzork@users.noreply.github.com>
2024-07-31 19:32:51 -0700

    Draw a red tile over items ready for harvest in farm area

--- StardewValley-FlashingHarvest/ModEntry.cs
+++ StardewValley-FlashingHarvest/ModEntry.cs
@@ -1,14 +1,51 @@
-﻿using StardewModdingAPI;
+﻿using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.TerrainFeatures;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace returnzork.StardewValleyMod.FlashingHarvest
 {
     internal class ModEntry : StardewModdingAPI.Mod
     {
-        public override void Entry(IModHelper helper) => throw new NotImplementedException();
+        bool isFlashingEnabled = false;
+        bool isActivelyFlashing = false;
+
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+            helper.Events.Display.RenderedWorld += Display_RenderedWorld;
+        }
+
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            if (e.Button == SButton.LeftStick || e.Button == SButton.O)
+                isFlashingEnabled = !isFlashingEnabled;
+        }
+
+        private void DrawTileOverlay(int xTile, int yTile)
+        {
+            // https://wiki.stardewvalley.net/Modding:Modder_Guide/Game_Fundamentals#Positions
+            Game1.DrawBox((xTile * Game1.tileSize) - Game1.viewport.X, (yTile * Game1.tileSize) - Game1.viewport.Y, Game1.tileSize, Game1.tileSize, Color.Red);
+        }
+
+        private void Display_RenderedWorld(object? sender, RenderedWorldEventArgs e)
+        {
+            if (!isFlashingEnabled)
+                return;
+            foreach (var feature in Game1.getFarm().terrainFeatures.Values)
+            {
+                if (feature is HoeDirt hd && hd.crop != null && hd.readyForHarvest())
+                {
+                    DrawTileOverlay((int)hd.crop.tilePosition.X, (int)hd.crop.tilePosition.Y);
+                }
+            }
+        }
     }
 }
